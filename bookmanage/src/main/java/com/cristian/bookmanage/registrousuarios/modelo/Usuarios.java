f055e041126738744e4fa03f3ee0b2ab390a3ceb
cristian <cregueiromartinez@danielcastelao.org>
2024-10-11 11:51:30 +0200

    creando la entidad usuarios para el login

--- bookmanage/src/main/java/com/cristian/bookmanage/registrousuarios/modelo/Usuarios.java
+++ bookmanage/src/main/java/com/cristian/bookmanage/registrousuarios/modelo/Usuarios.java
@@ -1,4 +1,67 @@
 package com.cristian.bookmanage.registrousuarios.modelo;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class Usuarios {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "email")
+    private String email;
+
+    public Usuarios() {
+    }
+
+    public Usuarios(String nombre, String password, String email) {
+        this.nombre = nombre;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Usuarios(Long id, String nombre, String password, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }

6abf75b534ab505d9fd98cffa0c01a0d29cea123
cristian <cregueiromartinez@danielcastelao.org>
2024-10-11 12:23:45 +0200

    creando la clase UsuarioServicioImpl

---
+++ bookmanage/src/main/java/com/cristian/bookmanage/registrousuarios/servicio/UsuarioServicio.java
@@ -0,0 +1,10 @@
+package com.cristian.bookmanage.registrousuarios.servicio;
+
+import com.cristian.bookmanage.registrousuarios.dto.UsuarioRegistroDTO;
+import com.cristian.bookmanage.registrousuarios.modelo.Usuarios;
+
+public interface UsuarioServicio {
+
+    public Usuarios save(UsuarioRegistroDTO registroDTO);
+
+}
---
+++ bookmanage/src/main/java/com/cristian/bookmanage/registrousuarios/servicio/UsuarioServicioImpl.java
@@ -0,0 +1,27 @@
+package com.cristian.bookmanage.registrousuarios.servicio;
+
+import com.cristian.bookmanage.registrousuarios.dto.UsuarioRegistroDTO;
+import com.cristian.bookmanage.registrousuarios.modelo.Usuarios;
+import com.cristian.bookmanage.registrousuarios.repositorio.UsuarioRepositorio;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioServicioImpl implements UsuarioServicio{
+
+    private UsuarioRepositorio usuarioRepositorio;
+
+    public UsuarioServicioImpl(UsuarioRepositorio usuarioRepositorio) {
+        this.usuarioRepositorio = usuarioRepositorio;
+    }
+
+    @Override
+    public Usuarios save(UsuarioRegistroDTO registroDTO) {
+        Usuarios usuario = new Usuarios(registroDTO.getId(),
+                registroDTO.getNombre(), registroDTO.getPassword(),
+                registroDTO.getEmail());
+
+        return usuarioRepositorio.save(usuario);
+    }
+
+
+}

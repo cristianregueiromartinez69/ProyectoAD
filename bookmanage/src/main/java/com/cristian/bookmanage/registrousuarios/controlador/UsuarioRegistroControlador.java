2109a409060c88a74ed3f069493edbc9c18d0c71
cristian <cregueiromartinez@danielcastelao.org>
2024-10-12 11:41:21 +0200

    cambiando funcionalidad a api rest, registro en base de datos funcionando

--- bookmanage/src/main/java/com/cristian/bookmanage/registrousuarios/controlador/UsuarioRegistroControlador.java
+++ bookmanage/src/main/java/com/cristian/bookmanage/registrousuarios/controlador/UsuarioRegistroControlador.java
@@ -2,37 +2,29 @@ package com.cristian.bookmanage.registrousuarios.controlador;
 
 import com.cristian.bookmanage.registrousuarios.dto.UsuarioRegistroDTO;
 import com.cristian.bookmanage.registrousuarios.servicio.UsuarioServicio;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/registro")
+@RestController
+@RequestMapping("/api/registro")
 public class UsuarioRegistroControlador {
 
-    private UsuarioServicio usuarioServicio;
+    private final UsuarioServicio usuarioServicio;
 
     public UsuarioRegistroControlador(UsuarioServicio usuarioServicio) {
         this.usuarioServicio = usuarioServicio;
     }
 
-    @ModelAttribute("usuario")
-    public UsuarioRegistroDTO retornarNuevoUsuarioRegistroDTO(){
-        return new UsuarioRegistroDTO();
-    }
 
-    @GetMapping
-    public String mostrarFormularioRegistro(){
-
-        return "registro";
-    }
 
     @PostMapping
-    public String registrarCuentaUsuario(@ModelAttribute("usuario") UsuarioRegistroDTO registroDTO){
-
+    public ResponseEntity<String> registrarCuentaUsuario(@RequestBody UsuarioRegistroDTO registroDTO) {
         usuarioServicio.save(registroDTO);
-        return "redirect:registro?exito";
+
+        return new ResponseEntity<>("Usuario registrado exitosamente", HttpStatus.CREATED);
     }
+
+
+
 }

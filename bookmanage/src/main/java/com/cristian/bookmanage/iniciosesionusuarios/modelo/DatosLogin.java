c35f8095140db85e7459575d5066633c8be7e5b8
cristian <cregueiromartinez@danielcastelao.org>
2024-10-13 13:01:23 +0200

    javadoc de los datos de inicio de sesion

--- bookmanage/src/main/java/com/cristian/bookmanage/iniciosesionusuarios/modelo/DatosLogin.java
+++ bookmanage/src/main/java/com/cristian/bookmanage/iniciosesionusuarios/modelo/DatosLogin.java
@@ -1,10 +1,21 @@
 package com.cristian.bookmanage.iniciosesionusuarios.modelo;
 
+/**
+ * Clase con los datos de inicio de sesion
+ * @author cristian
+ * @version 1.0
+ */
 public class DatosLogin {
 
+    //variables privadas que serán el correo y contraseña
     private String email;
     private String password;
 
+    /**
+     * Contructor de la clase
+     * @param email el email
+     * @param password la contraseña
+     */
     public DatosLogin(String email, String password) {
         this.email = email;
         this.password = password;
@@ -13,6 +24,10 @@ public class DatosLogin {
     public DatosLogin() {
     }
 
+    /**
+     * getters y setters de la clase
+     * @return el atributo correspondiente
+     */
     public String getEmail() {
         return email;
     }

b2a9b3b33701edc0cbdd9dab79db5ead4ae3ad4b
cristian <cr.m23@hotmail.com>
2024-10-13 11:31:17 +0200

    creando el controlador del login

--- bookmanage/src/main/java/com/cristian/bookmanage/iniciosesionusuarios/controlador/InicioSesionControlador.java
+++ bookmanage/src/main/java/com/cristian/bookmanage/iniciosesionusuarios/controlador/InicioSesionControlador.java
@@ -1,4 +1,40 @@
 package com.cristian.bookmanage.iniciosesionusuarios.controlador;
 
+import com.cristian.bookmanage.iniciosesionusuarios.modelo.DatosLogin;
+import com.cristian.bookmanage.iniciosesionusuarios.servicio.InicioSesionImpl;
+import com.cristian.bookmanage.iniciosesionusuarios.servicio.InicioSesionServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/login")
 public class InicioSesionControlador {
+
+    private InicioSesionServicio inicioSesionServicio;
+
+    @Autowired
+    public  InicioSesionControlador(InicioSesionServicio inicioSesionServicio){
+        this.inicioSesionServicio = inicioSesionServicio;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> login(@RequestBody DatosLogin datosLogin){
+
+        Boolean exitoLogin = inicioSesionServicio.checkUsuarios(datosLogin);
+
+        if(exitoLogin){
+            return new ResponseEntity<>("Inicio de sesión exitoso", HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>("Email o contraseña erroneos, vuelve a intentarlo", HttpStatus.UNAUTHORIZED);
+        }
+
+
+    }
+
 }

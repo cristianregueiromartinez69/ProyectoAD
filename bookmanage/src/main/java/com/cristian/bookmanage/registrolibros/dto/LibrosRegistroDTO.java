5258f864ef22ee290661dc1581b3553493e1ecd6
cristian <cregueiromartinez@danielcastelao.org>
2024-10-16 17:52:14 +0200

    javadoc de dot y entity de libros

--- bookmanage/src/main/java/com/cristian/bookmanage/registrolibros/dto/LibrosRegistroDTO.java
+++ bookmanage/src/main/java/com/cristian/bookmanage/registrolibros/dto/LibrosRegistroDTO.java
@@ -4,8 +4,14 @@ package com.cristian.bookmanage.registrolibros.dto;
 import java.time.LocalDate;
 import java.sql.Date;
 
+/**
+ * DT0 de la clase Libros que sirve de intermediaria entre la entidad y las demás capas de la aplicación
+ * @author cristian
+ * @version 1.0
+ */
 public class LibrosRegistroDTO {
 
+    //atributos privados de la clase
     private String isbn;
 
     private String autor;
@@ -16,6 +22,14 @@ public class LibrosRegistroDTO {
 
     private Date fechaRegistro;
 
+    /**
+     * Contructor de la clase
+     * @param isbn el isbn del libro que será la clave primaria
+     * @param autor el autor del libro
+     * @param nombre el nombre del libro
+     * @param fechaLectura la fecha de lectura del libro
+     * @param fechaRegistro la fecha en que fue registrado el libro
+     */
     public LibrosRegistroDTO(String isbn, String autor, String nombre, Date fechaLectura, Date fechaRegistro) {
         this.isbn = isbn;
         this.autor = autor;
@@ -29,6 +43,10 @@ public class LibrosRegistroDTO {
     }
 
 
+    /**
+     * getter y setter de la clase
+     * @return los atributos correspondientes
+     */
     public String getIsbn() {
         return isbn;
     }
--- bookmanage/src/main/java/com/cristian/bookmanage/registrolibros/modelo/Libros.java
+++ bookmanage/src/main/java/com/cristian/bookmanage/registrolibros/modelo/Libros.java
@@ -5,15 +5,24 @@ import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.sql.Date;
 
-
+/**
+ * Entidad libros que será una tabla de la base de datos
+ * con entity marcamos que será una tabla y con table(name=(nombre)) indicamos el nombre de la tabla
+ */
 @Entity
 @Table(name = "libros")
 public class Libros {
 
+    /**
+     * Indicamos que será la clave primaria con Id y que tiene que ser unica con unique=true, el nombre lo indicamos con name=(nombre)
+     */
     @Id
     @Column(unique = true, name = "isbn")
     private String isbn;
 
+    /**
+     * resto de columnas de la tabla con sus nombres
+     */
     @Column(name = "autor")
     private String autor;
 
@@ -26,6 +35,14 @@ public class Libros {
     @Column(name = "fecha_registro")
     private Date fechaRegistro;
 
+    /**
+     * Contructos de la clase libros
+     * @param isbn el isbn del libro
+     * @param autor el autor del libro
+     * @param nombre el nombre del libro
+     * @param fechaLectura la fecha de lectura del libro
+     * @param fechaRegistro la fecha de registro del libro
+     */
     public Libros(String isbn, String autor, String nombre, Date fechaLectura, Date fechaRegistro) {
         this.isbn = isbn;
         this.autor = autor;
@@ -38,6 +55,10 @@ public class Libros {
 
     }
 
+    /**
+     * getter y setter de la clase
+     * @return el valor correspondiente
+     */
     public String getIsbn() {
         return isbn;
     }

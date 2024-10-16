14e0b6b7e5a90eec7adfceead881690b79998075
cristian <cregueiromartinez@danielcastelao.org>
2024-10-16 17:54:30 +0200

    javadoc de repositorio y servicios de libros

--- bookmanage/src/main/java/com/cristian/bookmanage/registrolibros/repositorio/LibrosRepositorios.java
+++ bookmanage/src/main/java/com/cristian/bookmanage/registrolibros/repositorio/LibrosRepositorios.java
@@ -5,6 +5,11 @@ import com.cristian.bookmanage.registrolibros.modelo.Libros;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Interfaz repositorio donde hacemos los crud de los libros
+ * @author cristian
+ * @version 1.0
+ */
 @Repository
 public interface LibrosRepositorios extends JpaRepository <Libros, String> {
 
--- bookmanage/src/main/java/com/cristian/bookmanage/registrolibros/servicio/LibroServicio.java
+++ bookmanage/src/main/java/com/cristian/bookmanage/registrolibros/servicio/LibroServicio.java
@@ -3,7 +3,17 @@ package com.cristian.bookmanage.registrolibros.servicio;
 import com.cristian.bookmanage.registrolibros.dto.LibrosRegistroDTO;
 import com.cristian.bookmanage.registrolibros.modelo.Libros;
 
+/**
+ * Interfaz servicio de libros donde llamamos a los metodos correspondientes que gestionan las acciones sobre los libros
+ * @author cristian
+ * @version 1.0
+ */
 public interface LibroServicio {
 
+    /**
+     * Metodo que guardará los datos de los libros
+     * @param libroRegistroDTO los datos de los libros a través de la dto
+     * @return el objeto libros
+     */
     public Libros saveBooks(LibrosRegistroDTO libroRegistroDTO);
 }

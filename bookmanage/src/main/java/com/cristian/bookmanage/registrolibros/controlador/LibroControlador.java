599a6003cb82ebe2ba22b44755f8ba1ee58cb43b
cristian <cregueiromartinez@danielcastelao.org>
2024-10-16 17:47:29 +0200

    javadoc de la clase controlador de libros

--- bookmanage/src/main/java/com/cristian/bookmanage/registrolibros/controlador/LibroControlador.java
+++ bookmanage/src/main/java/com/cristian/bookmanage/registrolibros/controlador/LibroControlador.java
@@ -11,11 +11,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Clase rest controller que es un end point de swagger
+ * Comprobaremos si se realizan correctamente los registros de los libros
+ * @author cristian
+ * @version 1.0
+ */
 @RestController
 @RequestMapping("/api/libro")
 public class LibroControlador {
 
 
+    //inyectamos las dependecias en la variable de tipo LibroServicio y en la conexion de mongo
     @Autowired
     private LibroServicio libroServicio;
 
@@ -23,6 +30,11 @@ public class LibroControlador {
     private MongoConnectionService mongoConnectionService;
 
 
+    /**
+     * Metodo post mapping que va a enviar informacion al servidor para registrar un libro
+     * @param librosRegistroDTO los datos de los libros
+     * @return una respuesta dependiendo de si hubo o no exito a la hora de registrar el libro
+     */
     @PostMapping
     public ResponseEntity<String> registrarLibros(@RequestBody LibrosRegistroDTO librosRegistroDTO){
 

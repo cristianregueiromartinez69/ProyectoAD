e661c586c1e360b36ec29cbff8ce5f7396220632
cristian <cregueiromartinez@danielcastelao.org>
2024-10-19 14:24:07 +0200

    javadoc clase con registros de listas de libros en xml

--- bookmanage/src/main/java/com/cristian/bookmanage/registrolibros/dto/LibroRegistroDTOWrapper.java
+++ bookmanage/src/main/java/com/cristian/bookmanage/registrolibros/dto/LibroRegistroDTOWrapper.java
@@ -4,11 +4,22 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 
 import java.util.List;
 
+/**
+ * Clase que va a contener las listas de libros que se han de guardar en el xml
+ * la anotacion indica que el root se va a llamar libros
+ * @author cristian
+ * @version 1.0
+ */
 @JacksonXmlRootElement(localName = "Libros")
 public class LibroRegistroDTOWrapper {
 
+    //creamos una lista privada de tipo libros registros
     private List<LibrosRegistroDTO> libro;
 
+    /**
+     * Contructor de la clase
+     * @param libro el objeto de tipo libro
+     */
     public LibroRegistroDTOWrapper(List<LibrosRegistroDTO> libro) {
         this.libro = libro;
     }
@@ -16,6 +27,7 @@ public class LibroRegistroDTOWrapper {
     public LibroRegistroDTOWrapper() {
     }
 
+    //getters y setters de la clase
     public List<LibrosRegistroDTO> getLibro() {
         return libro;
     }
